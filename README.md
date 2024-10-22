6f033d3bdd3311b76cd54c83f8a926c79de13498
JPechberty <j.pechberty@gmail.com>
2024-10-22 22:44:46 +0200

    init repo

---
+++ README.md
@@ -0,0 +1,20 @@
+![separe](https://raw.githubusercontent.com/studoo-app/.github/main/profile/studoo-banner-logo.png)
+# JAVA EVAL1 SIO 1 : Coding Challenge 1
+[![Version](https://img.shields.io/badge/Version-2024-blue)]()
+
+## Travail à faire
+
+### Exercice 1 (5 points)
+Ecrire un programme qui permet calculer la puissance d'un nombre. 
+
+Le programme demande à l'utilisateur de saisir un nombre et sa puissance et affiche le résultat de la puissance du nombre saisi.
+### Exercice 2 (5 points)
+Ecrire un programme qui demande le nombre de kilomètres parcourus par un commercial et 
+qui calcule les frais de déplacement correspondants.
+
+### Exercice 3 (5 points)
+Ecrire un programme qui dessine un rectangle de largeur et de hauteur saisies par l'utilisateur.
+
+### Exercice 4 (5 points)
+A partir de l'index du compteur de l'ancien relevé et celui du nouveau relevé, 
+calculer la consommation d'électricité et le montant de la facture en euros sachant que le prix du kilowatt
---
+++ src/main/java/com/sio/Main.java
@@ -0,0 +1,67 @@
+package com.sio;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static final String GREEN = "\u001B[32m";
+    private static String RESET = "\u001B[0m";
+
+    public static void main(String[] args) {
+        System.out.println(GREEN);
+        System.out.println("#############################################");
+        System.out.println("            JAVA EVAL 1 SIO 1 ALT            ");
+        System.out.println("#############################################");
+
+        while(true){
+            System.out.println("#############################################");
+            System.out.println("1. Exercice 1");
+            System.out.println("2. Exercice 2");
+            System.out.println("3. Exercice 3");
+            System.out.println("4. Exercice 4");
+            System.out.println("0. Quitter le programme");
+            System.out.println("#############################################");
+            System.out.print("Entrer votre choix: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice){
+                case 1:
+                    System.out.println("---------------------------------------------");
+                    System.out.println("Exercice 1");
+                    System.out.println("---------------------------------------------");
+                    //TODO Exercice 1 : Entrer votre code ici
+                    System.out.println("---------------------------------------------");
+
+                    break;
+                case 2:
+                    System.out.println("---------------------------------------------");
+                    System.out.println("Exercice 2");
+                    System.out.println("---------------------------------------------");
+                    //TODO Exercice 2 : Entrer votre code ici
+                    System.out.println("---------------------------------------------");
+                    break;
+                case 3:
+                    System.out.println("---------------------------------------------");
+                    System.out.println("Exercice 3");
+                    System.out.println("---------------------------------------------");
+                    //TODO Exercice 3 : Entrer votre code ici
+                    System.out.println("---------------------------------------------");
+
+                    break;
+                case 4:
+                    System.out.println("---------------------------------------------");
+                    System.out.println("Exercice 4");
+                    System.out.println("---------------------------------------------");
+                    //TODO Exercice 4 : Entrer votre code ici
+                    System.out.println("---------------------------------------------");
+                    break;
+                case 0:
+                    System.out.println(RESET);
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+}
